ab787772b4965a371d69680c8d9fc8513055d864
Anders Lindmark <anders.lindmark@gmail.com>
2012-10-18 18:47:58 +0200

    Fixed gitwatcher

--- gitwatcher.sh
+++ gitwatcher.sh
@@ -11,7 +11,7 @@ cd $WORKDIR
 git fetch origin
 
 # Get list of changes
-LOG=`git log head..origin/master --oneline`
+LOG=`git log -- head..origin/master --oneline`
 
 # Check if anything has been done
 if [[ "$LOG" != "" ]]; then
