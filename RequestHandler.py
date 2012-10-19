4c6e5f922c4f6d88e01b3c52b9cf0445240ef14d
jimnys-8 <snaggadhagga@gmail.com>
2012-10-19 15:24:19 +0200

    Small changes, testing server

--- RequestHandler.py
+++ RequestHandler.py
@@ -4,6 +4,7 @@ import S3
 import Request
 from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
+import aws_common
 
 class RequestHandler(threading.Thread):
 
@@ -17,12 +18,12 @@ class RequestHandler(threading.Thread):
         request = Request.Request(body)
         reply = request.process()
         
-        print reply
+        #print reply
         
         # Write to S3
         url = S3.uploadFile(request.id, reply)
         
-        print url
+        #print url
         
         m = Message()
         m.set_body(url)
@@ -37,20 +38,20 @@ class RequestHandler(threading.Thread):
             #Sleep maybe?
             time.sleep(2)
 
-# The main method
+
 if __name__ == "__main__":
 
-    conn = SQSConnection('AKIAJJNNA44AWFOJ2PSA', '+mIhuaY8n6kEoJJ9rZjyMk2aMP0/2Se3c9kiCyx9')
-    inQueue = conn.create_queue('frontendInQueue')
-    outQueue = conn.create_queue('frontendOutQueue')
+    conn = SQSConnection(aws_common.AWS_ACCESS_KEY, aws_common.AWS_SECRET_KEY)
+    inQueue = conn.create_queue(aws_common.SQS_FRONTEND_IN)
+    outQueue = conn.create_queue(aws_common.SQS_FRONTEND_OUT)
 
     requestHandler = RequestHandler(inQueue, outQueue)
     requestHandler.start()
 
-    m = Message()
-    m.set_body(open('test3.xml', 'r').read())
-    print m.get_body()
-    status = inQueue.write(m)
+    #m = Message()
+    #m.set_body(open('test3.xml', 'r').read())
+    #print m.get_body()
+    #status = inQueue.write(m)
 
 #    for i in range(1, 5):
 #        m = Message()
