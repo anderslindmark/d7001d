1cc0a7751a41190a386c33cc081543de1affac39
Anders Lindmark <anders.lindmark@gmail.com>
2012-10-19 15:19:33 +0200

    Scripts for the request-handling server

---
+++ gitwatcherRequestHandler.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Put:
+#   */5 * * * * /path/to/gitwatcher
+# in crontab to check every five minutes
+
+WORKDIR=/home/ubuntu/project
+
+cd $WORKDIR
+# Update from remote
+git fetch origin
+
+# Get list of changes
+LOG=$(git log HEAD..origin/master)
+
+# Check if anything has been done
+if [[ "$LOG" != "" ]]; then
+	echo "There were new revisions."
+	echo -n "Stopping service... "
+	PID=`ps a | grep sensorDataHandler.py | grep -v grep | awk '{print $1}'`
+	if [[ "$PID" != "" ]]; then
+		kill $PID
+		echo "Done"
+	else
+		echo "No service found"
+	fi
+	
+	echo "Starting service"
+	$WORKDIR/runRequestHandler &
+else
+	echo "No new revisions"
+fi
+
