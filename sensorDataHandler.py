7b12c7b4bb3d4ffc0853b19de61d749c0be02bd9
Anders Lindmark <anders.lindmark@gmail.com>
2012-10-23 15:51:43 +0200

    sensorDataHandler now does sql-stuff in a dedicated thread. Hopefully...

--- sensorDataHandler.py
+++ sensorDataHandler.py
@@ -7,13 +7,26 @@ import os
 import errno
 import time
 
+from Queue import Queue
 from error import DataReadTimeoutException
 from datetime import datetime
 from struct import unpack
 
+class DatabaseWorker(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            params = self.queue.get(block=True)
+            cell_id, node_id, road_side, timestamp, size, raw_data = params
+            p = sql.Packet(cell_id, node_id, road_side, timestamp, size, raw_data)
+            self.queue.task_done()
+
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     """A server class that starts a new thread for each request it handles."""
-  
+
     def server_bind(self):
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # Set timeout in seconds
@@ -29,6 +42,8 @@ class SensorDataHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         """Handle incoming TCP traffic."""
+
+        print "My queue has %d items in it :D" % self.server.queue.qsize()
         
         # Read data from the network
         try:
@@ -68,7 +83,8 @@ class SensorDataHandler(SocketServer.BaseRequestHandler):
             logging.exception("Error receiving raw data");
         
         # Add the data to the database
-        p = sql.Packet(cell_id, node_id, road_side, timestamp, size, raw_data)
+        #p = sql.Packet(cell_id, node_id, road_side, timestamp, size, raw_data)
+        self.server.queue.put( (cell_id, node_id, road_side, timestamp, size, raw_data) )
         
         print "Data successfully stored on the database, probably."
         self.request.close()
@@ -122,8 +138,13 @@ if __name__ == "__main__":
     make_sure_path_exists('/home/ubuntu/logs/')
     logging.basicConfig(filename='/home/ubuntu/logs/sensorDataHandler.log', level=logging.ERROR)
 
+    db_queue = Queue()
+    db_worker = DatabaseWorker(db_queue)
+    db_worker.start()
+
     PORT = 9999
     server = ThreadedTCPServer(('', PORT), SensorDataHandler)
+    server.queue = db_queue
     try:
         server.serve_forever()
     except KeyboardInterrupt:
